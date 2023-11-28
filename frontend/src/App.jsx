e9f30a2f210b48358faa9191cd5a17b23e62114f
Calebbuffleben <calebbuffleben@gmail.com>
2023-11-28 19:35:47 -0300

    chat

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+   
+      <div className="flex h-screen bg-gray-100">
+        <div className="flex-1 flex flex-col overflow-hidden">
+          {/* Chat Header */}
+          <div className="flex-shrink-0 bg-white border-b-2 border-gray-200">
+            <div className="flex justify-between p-4">
+              <div className="text-lg font-semibold text-gray-800">Chat App</div>
+            </div>
+          </div>
+  
+          {/* Chat Messages */}
+          <div className="flex-1 overflow-x-hidden overflow-y-auto px-4 py-2">
+            {/* Messages go here */}
+            <div className="flex flex-col items-start space-y-2">
+              <div className="flex items-center">
+                <img
+                  src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'%3E%3Cpath stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M6 18L18 6M6 6l12 12'%3E%3C/path%3E%3C/svg%3E"
+                  alt="User 1"
+                  className="w-8 h-8 rounded-full mr-2"
+                />
+                <div className="bg-blue-500 text-white py-2 px-4 rounded-lg max-w-xs">
+                  Hello there!
+                </div>
+              </div>
+              <div className="flex items-center">
+                <img
+                  src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'%3E%3Cpath stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M6 18L18 6M6 6l12 12'%3E%3C/path%3E%3C/svg%3E"
+                  alt="User 2"
+                  className="w-8 h-8 rounded-full mr-2"
+                />
+                <div className="bg-gray-300 py-2 px-4 rounded-lg max-w-xs">
+                  How can I help you?
+                </div>
+              </div>
+            </div>
+          </div>
+  
+          {/* Chat Input */}
+          <div className="flex-shrink-0 bg-white border-t-2 border-gray-200">
+            <div className="flex p-4">
+              <input
+                type="text"
+                placeholder="Type a message..."
+                className="flex-1 border rounded-md p-2"
+              />
+              <button className="ml-2 bg-blue-500 text-white p-2 rounded-md">
+                Send
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
